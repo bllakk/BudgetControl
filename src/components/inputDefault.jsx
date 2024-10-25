ef7eae7602e3eec8eac57ebea146b4db2726aaf0
Gyan Monteiro <102252895+bjmontr@users.noreply.github.com>
2024-10-25 17:22:31 -0300

    tailwind

---
+++ src/components/inputDefault.jsx
@@ -0,0 +1,5 @@
+export default function InputDefault() {
+    return(
+        <input type="text" />
+    )
+}
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,19 @@
 export default function Login(){
     return(
-        <h3>Olá,</h3>
+        <div>
+            <h3>Olá, bem-vindo de volta</h3>
+        <form action="">
+            <div>
+                <span>Email</span>
+                <input type="email" />
+            </div>
+            <div>
+                <span>Senha</span>
+                <input type="password" />
+            </div>
+            <input type="submit" value="Entrar" />
+        </form>
+
+        </div>
     )
 }
