eee817700870ef1eaecd1a4708341d76ca6a7083
Gyan Monteiro <102252895+bjmontr@users.noreply.github.com>
2024-11-07 17:52:36 -0300

    styles

--- src/components/formLogin.jsx
+++ src/components/formLogin.jsx
@@ -1,15 +1,15 @@
 import InputDefault from "./inputDefault";
-
+import InputSubmit from "./inputSubmit";
 export default function FormLogin() {
     return (
         <div className="w-screen flex">
             <div className="w-[45%] h-screen">
-                <img className="object-cover w-full h-full rounded-r-lg" src="Login-Photo.jpeg" alt="Login" />
+                <img className="object-cover w-full h-full" src="Login-Photo.jpeg" alt="Login" />
             </div>
 
-            <div className="w-1/2 flex flex-col justify-center ">
+            <div className="w-1/2 flex flex-col items-center justify-center">
                 <h3 className="text-slate-200 mb-4 text-center">Olá, bem-vindo de volta</h3>
-                <form action="" className="">
+                <form action="" className="w-96">
                     <div className="flex flex-col text-left mb-4">
                         <span className="text-slate-200 mb-2">Email</span>
                         <InputDefault
@@ -24,8 +24,18 @@ export default function FormLogin() {
                             placeholder="Digite sua senha"
                         />
                     </div>
-                    <input className="text-slate-200 bg-blue-500 px-4 py-2 rounded w-full cursor-pointer" type="submit" value="Entrar" />
+                    <div className="flex justify-between mb-6">
+                        <div className="flex gap-2">
+                            <input type="checkbox" name="" id="" />
+                            <span className="text-slate-200">Lembrar email</span>
+                        </div>
+                        <a className="text-purple-alert" href="">Esquici minha senha</a>
+                    </div>
+                    <InputSubmit/>
                 </form>
+                <div className="mt-4">
+                    <p className="text-slate-200">Voce não tem uma conta? <a className="text-purple-alert" href="">Cadastre-se aqui.</a></p>
+                </div>
             </div>
         </div>
     );
---
+++ src/components/inputSubmit.jsx
@@ -0,0 +1,5 @@
+export default function InputSubmit(){
+    return(
+        <input className="text-slate-200 bg-purple-btn px-4 py-2 rounded w-full cursor-pointer" type="submit" value="Entrar" />
+    )
+}
