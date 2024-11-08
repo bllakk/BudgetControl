0673891bb7f59554431c633f152099f32626aded
Gyan Monteiro <102252895+bjmontr@users.noreply.github.com>
2024-11-08 17:24:52 -0300

    style

--- src/components/formLogin.jsx
+++ src/components/formLogin.jsx
@@ -4,37 +4,38 @@ export default function FormLogin() {
     return (
         <div className="w-screen flex">
             <div className="w-[45%] h-screen">
-                <img className="object-cover w-full h-full" src="Login-Photo.jpeg" alt="Login" />
+                <img className="object-cover w-full h-full" src="Login-Photo-1.png" alt="Login" />
             </div>
 
             <div className="w-1/2 flex flex-col items-center justify-center">
-                <h3 className="text-slate-200 mb-4 text-center">Olá, bem-vindo de volta</h3>
+                <h1 className="text-slate-200 mb-4 text-center font-tungstenMedium text-4xl">Olá, bem-vindo de volta</h1>
                 <form action="" className="w-96">
                     <div className="flex flex-col text-left mb-4">
-                        <span className="text-slate-200 mb-2">Email</span>
+                        <span className="text-slate-200 mb-2 font-poppins">Email</span>
                         <InputDefault
                             type="email"
                             placeholder="Digite seu e-mail"
                         />
                     </div>
                     <div className="flex flex-col text-left mb-4">
-                        <span className="text-slate-200 mb-2">Senha</span>
+                        <span className="text-slate-200 mb-2 font-poppins">Senha</span>
                         <InputDefault
                             type="password"
                             placeholder="Digite sua senha"
                         />
                     </div>
                     <div className="flex justify-between mb-6">
-                        <div className="flex gap-2">
+                        <div className="flex items-center gap-2">
                             <input type="checkbox" name="" id="" />
-                            <span className="text-slate-200">Lembrar email</span>
+                            <span className="text-slate-200 font-poppins text-sm">Lembrar email</span>
                         </div>
-                        <a className="text-purple-alert" href="">Esquici minha senha</a>
+                        <a className="text-purple-alert font-poppins " href="">Esqueci minha senha</a>
                     </div>
-                    <InputSubmit/>
+                    <InputSubmit
+                        value="Entrar"/>
                 </form>
                 <div className="mt-4">
-                    <p className="text-slate-200">Voce não tem uma conta? <a className="text-purple-alert" href="">Cadastre-se aqui.</a></p>
+                    <p className="text-slate-200 font-poppins">Voce não tem uma conta? <a className="text-purple-alert font-poppins" href="">Cadastre-se aqui.</a></p>
                 </div>
             </div>
         </div>
